3c3091ab582f7cd14669f9804bffe7efe7cf2bb0
Cooper Carlson <cjc.in.colorado@gmail.com>
2026-04-02 00:53:42 -0600

    Add feedback and test function

--- pair_programming.py
+++ pair_programming.py
@@ -1,6 +1,9 @@
-def mericaToMetric():
+def mericaToMetric(): ### feedback: fantastic function name
     feetString = input("How many feet? Do not enter units.  ")
+    ### feedback: function will be more useful if it does not require user inputs, maybe take arguments instead
     inchString = input("How many inches? Do not enter units.    ")
+
+    ### feedback: i usually like numpy datatypes for numerical calculations, but this works too. could also do float(input()) above instead
     feet = float(feetString)
     inches = float(inchString)
 
@@ -9,6 +12,8 @@ def mericaToMetric():
 
     return meters
 
+### feedback: might also include some case handling for negative inputs or non-numeric inputs, i.e. if your user enters a string it should tell them why that doesn't work.
+
 #main function included for testing convenience c:
 if __name__ == "__main__":
-    print(f"The result is: {mericaToMetric():.5f} meters")
+    print(f"The result is: {mericaToMetric():.5f} meters") # function works well for this use case!
